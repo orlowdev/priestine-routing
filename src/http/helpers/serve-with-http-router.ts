b9ac031b33046f8351d35fa5b97883a9e01f9ab8
priestine1 <priestine1.dev@gmail.com>
2018-12-08 23:11:59 +1000

    docs(serve helper): add annotation

--- src/http/helpers/serve-with-http-router.ts
+++ src/http/helpers/serve-with-http-router.ts
@@ -3,6 +3,12 @@ import { HttpPipeline } from '../http-pipeline';
 import { HttpRouter } from '../http-router';
 import { IHttpContext } from '../interfaces';
 
+/**
+ * Serve incoming HTTP requests using provided Router.
+ *
+ * @param {HttpRouter} router
+ * @returns {(request: IncomingMessage, response: ServerResponse) => Promise<void>}
+ */
 export const withHttpRouter = (router: HttpRouter) => (
   request: IncomingMessage,
   response: ServerResponse
