3bbabdebab8c1424b392d6c66613841ce6108fa3
priestine1 <priestine1.dev@gmail.com>
2019-02-03 15:26:31 +1000

    chore(route map): improve annotations

--- src/http/interfaces/http-pipeline.interface.ts
+++ src/http/interfaces/http-pipeline.interface.ts
@@ -5,6 +5,7 @@ import { PipelineInterface } from '@priestine/data/src';
  * Descriptor for HTTP pipeline.
  *
  * @interface HttpPipelineInterface
+ * @deprecated Will be removed in future releases
  */
 export interface HttpPipelineInterface<T = {}> extends PipelineInterface<T, IHttpContext> {
   concat(o: HttpPipelineInterface): HttpPipelineInterface;
